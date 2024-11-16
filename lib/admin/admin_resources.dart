0b714ee1ec4ee0a7b7cd4bf262f69342805dbbe4
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2024-11-16 12:23:36 -0800

    admin resouces updated

--- lib/admin/admin_resources.dart
+++ lib/admin/admin_resources.dart
@@ -9,8 +9,9 @@ class AdminResources extends StatefulWidget {
 }
 
 class _AdminResourcesState extends State<AdminResources> {
-  final pgFile =
+  final igFile =
   PdfControllerPinch(document: PdfDocument.openAsset('assets/CH1-IG.pdf'));
+  final slides = PdfControllerPinch(document: PdfDocument.openAsset('assets/CH1-Slides.pdf'));
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +26,7 @@ class _AdminResourcesState extends State<AdminResources> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => PdfViewPinch(controller: pgFile)),
+                      builder: (context) => PdfViewPinch(controller: igFile)),
                 );
               },
               child: const Card(
@@ -40,12 +41,56 @@ class _AdminResourcesState extends State<AdminResources> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            "Participant Guide",
+                            "Instructor Guide",
                             style: TextStyle(
                                 fontSize: 24, fontWeight: FontWeight.bold),
                           ),
                           Text(
                             "Your handbook for the next chapter.",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                      Icon(
+                        Icons.chevron_right,
+                        size: 50,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Container(
+            height: 150,
+            margin: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => PdfViewPinch(controller: slides)),
+                );
+              },
+              child: const Card(
+                color: Color.fromRGBO(204, 234, 211, 0.5),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Instructor Slides",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Get ready for the next class",
                             style: TextStyle(fontSize: 15),
                           )
                         ],
